dce59a15c36c7bfd96d6ebfd6091fd4ff087f896
Matt Robinson <mthw.wm.robinson@gmail.com>
2018-07-04 12:21:46 -0400

    switched body to serif

--- src/components/Section.js
+++ src/components/Section.js
@@ -7,29 +7,29 @@ class Section extends Component {
   render() {
     return (
       <div className="Section">
-            <Col xs={12} sm={12} md={3} lg={3} className='section-title'>
-              <b>join the flock</b>
-              <hr/>
-              <img
-                src='/dataflock-duck.png'
-                alt='Dataflock Duck'
-                height='90px'
-              />
-              <img
-                src='/dataflock-duck.png'
-                alt='Dataflock Duck'
-                height='90px'
-              />
-              <img
-                src='/dataflock-duck.png'
-                alt='Dataflock Duck'
-                height='90px'
-              />
-            </Col>
-            
-            <Col xs={12} sm={12} md={9} lg={9} className='section-content'>
-              <b><p>Have a lot of data? Not sure to do with it? The flock is here to help! Dataflock offers a variety of services, ranging from data consulting to enterprise application development. We take care of the boring stuff so that you can do what you do best.</p></b>
-            </Col>
+          <Col xs={12} sm={12} md={3} lg={3} className='section-title'>
+            <b>join the flock</b>
+            <hr/>
+            <img
+              src='/dataflock-duck.png'
+              alt='Dataflock Duck'
+              height='90px'
+            />
+            <img
+              src='/dataflock-duck.png'
+              alt='Dataflock Duck'
+              height='90px'
+            />
+            <img
+              src='/dataflock-duck.png'
+              alt='Dataflock Duck'
+              height='90px'
+            />
+          </Col>
+          
+          <Col xs={12} sm={12} md={9} lg={9} className='section-content'>
+            <b><p>Have a lot of data? Not sure to do with it? The flock is here to help! Dataflock offers a variety of services, ranging from data consulting to enterprise application development. We take care of the boring stuff so that you can do what you do best.</p></b>
+          </Col>
       </div>
     );
   }
